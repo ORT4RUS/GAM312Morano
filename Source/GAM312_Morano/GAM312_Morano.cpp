b008d2add0e1b2dd30c4bcdb6cf4bd347451c9fc
ORT4RUS <chris.morano88@gmail.com>
2026-05-10 12:21:05 -0600

    Initial Commit

---
+++ Source/GAM312_Morano/GAM312_Morano.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM312_Morano.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM312_Morano, "GAM312_Morano" );
