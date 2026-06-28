d0f7f8a5272f5f477b09999eb4a978e9a59b3110
ORT4RUS <chris.morano88@gmail.com>
2026-06-28 15:52:59 -0600

    Clean Unreal project submission

---
+++ Source/GAM312_Morano/MyGAM312PjctGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyGAM312PjctGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM312_MORANO_API AMyGAM312PjctGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
